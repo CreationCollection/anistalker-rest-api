7ea7e67791a80b9b55248d7832ad1348e6b28eb3
CreationCollection <creationcollection011@gmail.com>
2023-09-19 14:15:52 +0530

    Convert project into typescript

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-const express = require("express")
+import express, { Request, Response } from "express"
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
     res.send("Congratulations! You have reached the home page!")
 })
 
