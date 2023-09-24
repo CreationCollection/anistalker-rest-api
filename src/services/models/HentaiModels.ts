96a9aa2106a183d2bfb781f5efa05883c32c1a62
CreationCollection <creationcollection011@gmail.com>
2023-09-24 23:24:13 +0530

    Add Handling for since timestamps in hentai model

--- src/services/models/HentaiModels.ts
+++ src/services/models/HentaiModels.ts
@@ -5,7 +5,7 @@ export class HentaiTag {
         public name: string = "",
         public desc: string = "",
         public images: { short: string, tall: string } = { short: '', tall: '' },
-    ) {}
+    ) { }
 }
 
 export class Hentai {
@@ -20,7 +20,7 @@ export class Hentai {
         public likes: number = 0,
         public rank: number = 0,
         public rating: number = 0,
-    ) {}
+    ) { }
 }
 
 export class HentaiStream {
@@ -31,7 +31,7 @@ export class HentaiStream {
         public filesize: number = 0,
         public filename: string = '',
         public url: string = '',
-    ) {}
+    ) { }
 }
 
 export class HentaiVideo {
@@ -43,19 +43,27 @@ export class HentaiVideo {
         public next_video: Hentai = new Hentai(),
         public random_video: Hentai = new Hentai(),
         public video_streams: HentaiStream[] = [],
-    ) {  }
+    ) { }
 }
 
 export class HentaiDate {
-    constructor(public year: number = 0, public month: number = 0, public day: number = 0) {}
+    constructor(public year: number = 0, public month: number = 0, public day: number = 0) { }
 
     static toDate(date: string): HentaiDate {
         let d = new HentaiDate()
-        let dates = date.split('T')[0].split('-')
-        d.year = parseInt(dates[0])
-        d.month = parseInt(dates[1])
-        d.day = parseInt(dates[2])
 
+        if (Number(date)) {
+            let hd = new Date((new Date()).getTime() - parseInt(date))
+            d.year = hd.getFullYear()
+            d.month = hd.getMonth()
+            d.day = hd.getDay()
+        }
+        else {
+            let dates = date.split('T')[0].split('-')
+            d.year = parseInt(dates[0])
+            d.month = parseInt(dates[1])
+            d.day = parseInt(dates[2])
+        }
         return d
     }
 }
