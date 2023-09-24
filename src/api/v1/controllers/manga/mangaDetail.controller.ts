ab18e899664235a1d704b285d97c7f0a8ee44bdd
CreationCollection <creationcollection011@gmail.com>
2023-09-24 23:45:08 +0530

    make manga controllers safe execute

--- src/api/v1/controllers/manga/mangaDetail.controller.ts
+++ src/api/v1/controllers/manga/mangaDetail.controller.ts
@@ -4,28 +4,36 @@ import { safeExecute } from '../../../utils/safeUtils.js'
 import { Request, Response } from 'express'
 
 const getMangaInfo = async (req: Request, res: Response) => {
-    let mangaId = req.params.mangaId
-    let data = await MangaDex.getMangaInfo(mangaId)
-    res.json({ status: 200, data: data ?? {  } })
+    safeExecute(async () => {
+        let mangaId = req.params.mangaId
+        let data = await MangaDex.getMangaInfo(mangaId)
+        res.json({ status: 200, data: data ?? {} })
+    }, res)
 }
 
 const getMangaCover = async (req: Request, res: Response) => {
-    let coverId = req.params.coverId
-    let data = await MangaDex.getMangaCover(coverId)
-    res.json({ status: 200, data })
+    safeExecute(async () => {
+        let coverId = req.params.coverId
+        let data = await MangaDex.getMangaCover(coverId)
+        res.json({ status: 200, data })
+    }, res)
 }
 
 const getChapters = async (req: Request, res: Response) => {
-    let mangaId = req.params.mangaId
-    let page = req.query.page && typeof req.query.page == "string" ? parseInt(req.query.page) : 1
-    let data = await MangaDex.getChapters(mangaId, page)
-    res.json({ status: 200, data })
+    safeExecute(async () => {
+        let mangaId = req.params.mangaId
+        let page = req.query.page && typeof req.query.page == "string" ? parseInt(req.query.page) : 1
+        let data = await MangaDex.getChapters(mangaId, page)
+        res.json({ status: 200, data })
+    }, res)
 }
 
 const getChapterPages = async (req: Request, res: Response) => {
-    let chapterId = req.params.chapterId
-    let data = await MangaDex.getChapterPages(chapterId)
-    res.json({ status: 200, data })
+    safeExecute(async () => {
+        let chapterId = req.params.chapterId
+        let data = await MangaDex.getChapterPages(chapterId)
+        res.json({ status: 200, data })
+    }, res)
 }
 
 export default { getMangaInfo, getMangaCover, getChapters, getChapterPages }
