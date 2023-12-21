6593f2f1f4f5dac3eef60c9f84c3a1e787e1bb27
Creation Collection <creationcollection011@gmail.com>
2023-12-21 16:39:19 +0530

    feat (MasterZoro): Add function to get better streaming video links

--- src/services/providers/anime/MasterZoro.ts
+++ src/services/providers/anime/MasterZoro.ts
@@ -1,8 +1,10 @@
+import { Video } from "src/services/models/VideoModels.js";
 import { AnimeCategory, AnimeEpisodeDetail, AnimeEpisodeServers, AnimeFull, AnimeSpotlight, IAnimePage, ZoroStreamData } from "../../models/AnimeModels.js";
 import { ZoroInformer } from "./informers/ZoroInformer.js";
 import { ZoroFilter, ZoroSearch } from "./informers/ZoroSearch.js";
 import { ZoroSpotlight } from "./informers/ZoroSpotlight.js"
 import { ZoroStream } from "./streamers/ZoroStream.js"
+import { GogoStream } from './streamers/GogoStream.js'
 
 export class MasterZoro {
     static search(search: string, _filter?: ZoroFilter | null): IAnimePage { return ZoroSearch.search(search, _filter) }
@@ -18,4 +20,6 @@ export class MasterZoro {
     static getEpisodes = async (animeId: number): Promise<AnimeEpisodeDetail[]> => ZoroStream.getEpisodes(animeId)
     static getEpisodeServers = async (episodeId: number): Promise<AnimeEpisodeServers> => ZoroStream.getServersForEpisode(episodeId)
     static getEpisodeVideo = async (episodeServerId: number, seperateFiles: boolean = false): Promise<ZoroStreamData> => ZoroStream.getVideoData(episodeServerId, seperateFiles)
+
+    static getEpisodeVideoLinks = async (zoroId: number, episode: number, lang: 'sub' | 'dub', seperateFiles: boolean = false): Promise<Video> => GogoStream.getVideoData(zoroId, episode, lang, seperateFiles)
 }
