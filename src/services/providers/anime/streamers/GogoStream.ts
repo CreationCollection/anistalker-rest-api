c78b53a4d386f2d4115f87a5032bd2604682e61b
Creation Collection <creationcollection011@gmail.com>
2023-12-22 14:21:13 +0530

    fix (GogoStream): Fix wrong episode id

--- src/services/providers/anime/streamers/GogoStream.ts
+++ src/services/providers/anime/streamers/GogoStream.ts
@@ -6,6 +6,7 @@ import { Video } from "../../../models/VideoModels.js";
 
 export class GogoStream {
     static readonly baseUrl = 'https://gogoanime3.net'
+    static readonly episodeAjax = 'https://ajax.gogo-load.com/ajax/load-list-episode'
 
     static async getVideoData(
         zoroId: number, 
@@ -16,18 +17,19 @@ export class GogoStream {
         const map = await ZoroMapper.mapZoro(zoroId)
         const animeId = lang == 'sub' ? map.gogoSub : map.gogoDub
 
-        if (animeId == null) throw new Error("No Anime Found for ID: " + zoroId)
-
-        const episodeId = `${animeId}-episode-${episode}`
-        const videoUrl = await this.getStreamUrl(episodeId)
+        if (animeId.media == null) throw new Error("No Anime Found for ID: " + zoroId)
+        const videoUrl = await this.getStreamUrl(animeId.media, episode)
 
         if (videoUrl == null) throw new Error('No Source found for episode ' + episode)
 
         return GogoCdn.extract(videoUrl, seperateFiles)
     }
 
-    static async getStreamUrl(id: string): Promise<string | null> {
-        const res = await axios.get(`${this.baseUrl}/${id}`)
+    static async getStreamUrl(mediaId: number, episode: number): Promise<string | null> {
+        const episodeId = (await axios.get(`${this.episodeAjax}?ep_start=${episode}&ep_end=${episode}&id=${mediaId}`)).data.match(/\/(.*?)(?=")/)[0]
+        console.log(this.baseUrl + episodeId)
+        
+        const res = await axios.get(this.baseUrl + episodeId)
         const $ = load(res.data)
         const url = $('#load_anime > div > div > iframe').attr('src')
         return url ?? null
